3cb208b6d731d362b29502d312e31ee2ffb34391
huss2012 <huss24940@gmail.com>
2024-08-03 10:18:15 +0300

    Phase 1 & 2 are done

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,18 @@
 -- Your code here
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS coffee_orders;
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(40) NOT NULL,
+    phone NUMRIC(10) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    points INTEGER DEFAULT 5,
+    created_at TIMESTAMP DEFAULT CURRENT_TIME
+);
+
+CREATE TABLE coffee_orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    is_redeemed BOOLEAN DEFAULT "not redeemed",
+    ordered_at TIMESTAP DEFAULT CURRENT_TIME
+);
